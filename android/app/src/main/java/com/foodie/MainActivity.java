6ec6454d078d65088a832e4bbb0ed83cf0c06f44
hieu <hieu@macoss-MacBook-Pro.local>
2021-02-24 08:52:40 +0700

    onboard and splash screens

--- android/app/src/main/java/com/foodie/MainActivity.java
+++ android/app/src/main/java/com/foodie/MainActivity.java
@@ -1,4 +1,4 @@
-package com.portfolio;
+package com.foodie;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Portfolio";
+    return "Foodie";
   }
 }
---
+++ src/styles/common.ts
@@ -0,0 +1,5 @@
+import { StyleProp, ViewStyle } from "react-native";
+
+export const container: StyleProp<ViewStyle>  = {
+    flex: 1
+}
